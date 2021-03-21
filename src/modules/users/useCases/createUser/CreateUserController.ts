a1ec6f561d4dc9a0e49ec61c5f84a974375245ad
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-21 00:22:52 -0400

    Implements CreateUserController

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,11 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { email, name } = request.body;
+
+    this.createUserUseCase.execute({ email, name });
+
+    return response.status(201).send();
   }
 }
 
