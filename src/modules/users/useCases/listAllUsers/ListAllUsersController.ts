d6137a602ebec2b9ee3d4d0ee5ef15999da7bf50
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-21 00:36:15 -0400

    Implements ListAllUsersController

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    const users = this.listAllUsersUseCase.execute({
+      user_id: user_id as string,
+    });
+
+    return response.json(users);
   }
 }
 
