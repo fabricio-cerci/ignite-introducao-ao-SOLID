136b277680d1adfcd25fd7c4e16041a0969f0d74
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-21 00:48:26 -0400

    Implements TurnUserAdminUseCase

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("Usuário não encontrado");
+    }
+
+    const updatedUser = this.usersRepository.turnAdmin(user);
+
+    return updatedUser;
   }
 }
 
