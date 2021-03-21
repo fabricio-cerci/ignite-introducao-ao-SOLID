6ad0add59f3618c314efe3329577a277d5697f74
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-21 00:26:33 -0400

    Implements findById method on UsersRepository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -34,7 +34,9 @@ class UsersRepository implements IUsersRepository {
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
